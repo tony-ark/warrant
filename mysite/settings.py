8451199b188763759a5538d7f10a475df106924e
tony-ark <aarkorful002@gmail.com>
2025-07-18 15:56:21 -0700

    Update database settings and add initial migration for Product and Client models

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "warrant",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
--- project/models.py
+++ project/models.py
@@ -3,17 +3,13 @@ from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    tag = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
-    description = models.TextField()
-    img = models.TextField()
+    info = models.TextField()
     warranty = models.IntegerField()
 
 class Client(models.Model):
-    client_id = models.AutoField(primary_key=True)
     client_name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
 
